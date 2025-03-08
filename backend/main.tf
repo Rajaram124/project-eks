46fd5d1f47f75e7c7805b936762aa06254e7a7ec
Rajaram124 <149313776+Rajaram124@users.noreply.github.com>
2025-03-08 17:58:11 +0530

    Update main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-eks-s3-bucket"
+  bucket = "inditown-terraform-state-eks-s3-bucket-test"
 
   lifecycle {
     prevent_destroy = false
